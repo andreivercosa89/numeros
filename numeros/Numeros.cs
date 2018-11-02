f5cd757dc931e903b30da665f56f011bba211e6b
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-01 21:56:13 -0400

    sequencias de numeros

---
+++ numeros/Numeros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace numeros
+{
+    public class Numeros
+    {
+        public int incrementar()
+        {
+            int a = 1;
+            for (int i = 1; i < 10; i++ ){
+                i+=i;
+                return i;
+            }
+            return a;
+        }
+    }
+}
---
+++ numeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text= "";
+            for (int i = 1; i <= 10; i++)
+            {
+
+                for (int a = 1; a <= 10; a++)
+                {
+                    text += a + " ";
+                }
+                Console.WriteLine(i + ", " + "(" + text + ")");
+                text = "";
+            }
+
+
+        }
+    }
+}
